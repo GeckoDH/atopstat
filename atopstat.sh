4c5274ca7e07e8750392047d35c6f2d3995fb961
Daniel Graf <daniel@debian-hell.org>
2014-11-20 10:13:59 +0100

    First commit ;-)

---
+++ atopstat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  atopstat.sh
+# 
+#         USAGE:  ./atopstat.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Daniel Graf (DG), daniel@debian-hell.org
+#       COMPANY:  ---
+#       VERSION:  1.0
+#       CREATED:  11/20/2014 10:12:28 AM CET
+#      REVISION:  ---
+#===============================================================================
+
+
